163689c4f04002e9eea6ac440eee87257ec5941f
masaponto <masaponto@gmail.com>
2019-10-12 18:18:47 +0900

    [Add] base component

---
+++ src/components/DigDoc.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="app">
+        <div class="Title">
+            <h1>Dig Doc</h1>
+        </div>
+        <div class="Input">
+            <input type="text" v-model="keyword">
+        </div>
+        <div class="DocTable">
+            <table align="center">
+                <tr v-for="(doc, index) in filterdDocs" :key="index">
+                    <td v-text="doc.id"></td>
+                    <td v-text="doc.name"></td>
+                    <td v-text="doc.url"></td>
+                    <td v-text="doc.update"></td>
+                    <td v-text="doc.note"></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+ export default {
+     data: function(){
+         return {
+             keyword: '',
+             docs: [
+                 {
+                     id: 1,
+                     name: "寿司レシピ",
+                     url: "masaponto.github.io",
+                     update: 20190909,
+                     note: "aaa"
+                 },
+                 {
+                     id: 2,
+                     name: "寿司",
+                     url: "masaponto.github.io",
+                     update: "20190909",
+                     note: 'bbb'
+                 },
+                 {
+                     id: 3,
+                     name: "PJCD",
+                     url: "masaponto.github.io",
+                     update: "20190909",
+                     note: 'bbb'
+                 }
+             ]
+         }
+     },
+     computed: {
+         filterdDocs: function() {
+             let docs = this.docs;
+             let keyword = this.keyword;
+             if(keyword) {
+                 docs = docs.filter(function(doc) {
+                     return doc.name.indexOf(keyword) !== -1;
+                 });
+             }
+             return docs;
+         }
+     }
+ }
+</script>
+
+<style>
+</style>
