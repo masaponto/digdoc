2ddd4168e206f8fd25beeaa1c23bc1b27e27b8eb
masaponto <masaponto@gmail.com>
2019-10-12 22:26:40 +0900

    [Add] table columns

--- src/components/DigDoc.vue
+++ src/components/DigDoc.vue
@@ -8,13 +8,22 @@
         </div>
         <div class="DocTable">
             <table align="center">
-                <tr v-for="(doc, index) in filterdDocs" :key="index">
-                    <td v-text="doc.id"></td>
-                    <td v-text="doc.name"></td>
-                    <td v-text="doc.url"></td>
-                    <td v-text="doc.update"></td>
-                    <td v-text="doc.note"></td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th v-for="(value, index) in columns" :key="index">
+                            {{value}}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(doc, index) in filterdDocs" :key="index">
+                        <td v-text="doc.id"></td>
+                        <td v-text="doc.name"></td>
+                        <td v-text="doc.url"></td>
+                        <td v-text="doc.note"></td>
+                        <td v-text="doc.update"></td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
@@ -27,7 +36,8 @@
      data: function(){
          return {
              keyword: '',
-             docs: docsMaster
+             docs: docsMaster,
+             columns: ["id", "ファイル名", "URL", "備考", "最終更新"]
          }
      },
      computed: {
@@ -46,4 +56,31 @@
 </script>
 
 <style>
+ table {
+     border: 2px solid #42b983;
+     border-radius: 3px;
+     background-color: #fff;
+ }
+ th {
+     background-color: #42b983;
+     color: rgba(255,255,255,0.66);
+     cursor: pointer;
+     -webkit-user-select: none;
+     -moz-user-select: none;
+     -ms-user-select: none;
+     user-select: none;
+ }
+ td {
+     background-color: #f9f9f9;
+ }
+ th, td {
+     min-width: 30px;
+     padding: 10px 20px;
+ }
+ th.active {
+     color: #fff;
+ }
+ th.active .arrow {
+     opacity: 1;
+ }
 </style>
